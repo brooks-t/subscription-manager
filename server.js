e29586a9be7acae14e6287206e5159a84981960d
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-05-03 10:13:20 -0700

    created basic file structure and added boiler plate code

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const sequelize = require("./config/connection")
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const User = require("./models/User")
+require("dotenv").config();
+
+const routes = require("./controllers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"))
+// TODO: what's difference from below and above for?
+// app.use(express.static(path.join(__dirname, 'public'))); 
+
+
+const hbs = exphbs.create({})
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(
+  {
+    secret:process.env.SESSION_SECRET, // TODO: did I set this up correction in the .env file?
+    resave:false,
+    cookie:{
+      maxAge:1000*60*60*2 // TODO: sets cookie expriation two hours (should we use this?)
+    },
+    saveUninitialized:true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  }
+))
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
