a99d93c67bbb39dd8ad3653de864ed8bad61ba0e
Aurora Duncan <aurorabrynn@gmail.com>
2021-05-04 13:26:00 -0700

    edited addsub and added editsub

--- public/js/addsub.js
+++ public/js/addsub.js
@@ -1,17 +1,17 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
 
-    const name = document.querySelector('#addSubName').value.trim();
+    const title = document.querySelector('#addSubName').value.trim();
     const category = document.querySelector('#addSubCategory').value.trim();
-    const payment = document.querySelector('#addSubNextPay').value.trim();
-    const cycle = document.querySelector('#addSubCycle').value.trim();
+    const next_payment = document.querySelector('#addSubNextPay').value.trim();
+    const billing_cycle = document.querySelector('#addSubCycle').value.trim();
     const amount = document.querySelector('#addSubAmount').value.trim();
     const rating = document.querySelector('#addSubRating').value.trim();
 
-    if (name && category && payment && cycle && amount && rating) {
+    if (title && category && next_payment && billing_cycle && amount && rating) {
         const response = await fetch(`/api/subscriptions`, {
             method: 'POST',
-            body: JSON.stringify({ name, category, payment, cycle, amount, rating }),
+            body: JSON.stringify({ title, category, next_payment, billing_cycle, amount, rating }),
             headers: {
                 'Content-Type': 'application/json',
             },
