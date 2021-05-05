70641de3b16e095ff10e108f65ecf351e9efa33d
Juan Vazquez <ehulises28@hotmail.com>
2021-05-05 14:50:55 -0700

    added utils folder,subroutes,userroutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-/* const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const userRoutes = require("./userRoutes");
+const subRoutes = require("./subRoutes");
 
-router.use('/users', userRoutes);
-router.use('/projects', projectRoutes); */
+router.use("/users", userRoutes);
+router.use("/subscriptions", subRoutes);
 
 module.exports = router;
