f1af1ee09bf442ef00bf4f7dc7598c8dae68baf3
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-05-05 14:33:28 -0700

    separated signup js file into login.js file - created utils folder withAuth - renamed signin to login for consistency - fixed links

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Subscription = require("../models/Subscription");
 const bcrypt = require("bcrypt");
+const withAuth = require("../utils/auth");
 
 router.get('/signup', (req, res) => {
   // If the user is already logged in, redirect the request to another route
@@ -11,6 +13,27 @@ router.get('/signup', (req, res) => {
   res.render('signup');
 });
 
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Subscription }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('dashboard', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -18,8 +41,11 @@ router.get('/login', (req, res) => {
     return;
   }
 
-  res.render('signin');
+  res.render('login');
 });
+
+
+
 
 module.exports = router;
 
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,24 +1,3 @@
-const loginFormHandler = async (event) => {
-  event.preventDefault();
-
-  const email = document.querySelector('#loginEmail').value.trim();
-  const password = document.querySelector('#loginPassword').value.trim();
-
-  if (email && password) {
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
@@ -40,10 +19,6 @@ const signupFormHandler = async (event) => {
     }
   }
 };
-
-document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
 
 document
   .querySelector('.signup-form')
