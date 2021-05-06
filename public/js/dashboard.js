2653262a50e4bfe0dc0051ab2132a36ec9d0b64c
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-05-06 12:41:39 -0700

    sub cards now appear on dashboard. logout button added and logs user out. formatted date on card with helpers.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,3 +5,20 @@ for (i = 0; i < title.length; i++) {
     const firstLetter = title[i].innerHTML.charAt(0)
     div[i].append(firstLetter)
 }
+
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/login');
+    } else {
+      alert(response.statusText);
+    }
+  };
+
+document
+    .querySelector('#logoutBtn')
+    .addEventListener('click', logout);
