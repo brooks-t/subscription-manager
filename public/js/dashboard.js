f3d867f65ed6355372711fd1f9c087d00b250610
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-05-06 16:21:55 -0700

    connected the sub cards to the edit page

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -8,18 +8,38 @@ for (i = 0; i < title.length; i++) {
 
 const logout = async () => {
     const response = await fetch('/api/users/logout', {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
     });
-  
-    if (response.ok) {
-      document.location.replace('/login');
-    } else {
-      alert(response.statusText);
-    }
-  };
 
+    if (response.ok) {
+        document.location.replace('/login');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+const editSub = async (event) => {
+    event.stopPropagation();
+
+    const response = await fetch(`/editsub/${id}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace(`/editsub/${id}`);
+    } else {
+        alert(response.statusText);
+    }
+};
 
 document
     .querySelector('#logoutBtn')
     .addEventListener('click', logout);
+
+document
+    .querySelectorAll('.substers').forEach( sub => {
+        sub.addEventListener('click', editSub);
+    })
+    
