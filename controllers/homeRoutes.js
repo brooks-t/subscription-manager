a82b58a2eb3113b8ec99621edee10f0a44462e96
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-05-07 15:06:52 -0700

    added home '/' route that directs users to signup page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,6 +4,15 @@ const Subscription = require("../models/Subscription");
 const bcrypt = require("bcrypt");
 const withAuth = require("../utils/auth");
 
+router.get('/', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('signup');
+});
+
 router.get('/signup', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
